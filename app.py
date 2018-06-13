4bb450ca2e78476f1cf1c2d4fed0ae06be14a44a
quantaquadri <abdulquadrisotomiwa@gmail.com>
2018-06-13 16:38:29 +0000

    giftslist page created and links and images being worked on

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask,render_template
 app = Flask(__name__)
  
-@app.route("/")
-def hello():
-    return "Hello World!"
 
-@app.route("/abdul")
+@app.route("/gifts")
 def abdul():
- 	return render_template('index.html')
+ 	return render_template('gift_list.html')
+
+@app.route("/cart")
+def cart():
+ 	return render_template('cart.html')
  
 if __name__ == "__main__":
     app.run(debug = True)
